7a8d2551a1d349c12422dc93dec56ba0b66d85d4
CodeWithShivam45 <prajapatishivam55296@gmail.com>
2024-04-21 15:49:20 +0530

    Javascript

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,32 +1,40 @@
-// console.log('Hello');
-
-let value = document.getElementById('value');
-
+let value = document.getElementById("value");
 
 const numberClick = (val) => {
-    if(value.textContent == 0 || value.textContent === ''){
-        value.textContent = '';
-    }
-    value.textContent+=val;
-    
-}
+  if (value.textContent == 0 || value.textContent === "") {
+    value.textContent = "";
+  }
+  value.textContent += val;
+};
 
-const clear = () =>{
-    console.log("Hello");
-    value.textContent = '0';
-}
+const clear = () => {
+  value.textContent = "0";
+};
 
-const del = () =>{
-    console.log(value.textContent);
-    value.textContent = value.textContent.slice(0,-1);
-}
+const del = () => {
+  value.textContent = value.textContent.slice(0, -1);
+  if (value.textContent == "") {
+    value.textContent = "0";
+  }
+};
 
 const calculate = () => {
-    console.log('Cal');
-    value.textContent = eval(value.textContent);
-}
+  value.textContent = eval(value.textContent);
+};
 
-document.getElementById('clear').addEventListener('click',clear);
-document.getElementById('delete').addEventListener('click',del);
-document.getElementById('calculate').addEventListener('click',calculate);
+document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("delete").addEventListener("click", del);
+document.getElementById("calculate").addEventListener("click", calculate);
 
+let form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+form.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  if (e.key == "Enter") {
+    e.preventDefault();
+    calculate();
+  }
+});
